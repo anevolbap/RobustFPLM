882c073b1d63bb7c11d6edef59416be0090cdcf4
anevolbap <pvena1@gmail.com>
2020-10-21 12:34:20 -0300

    Remove all criteria but the one used in the paper (renamed as rbic) and improved documentation

---
+++ R/rbic.R
@@ -0,0 +1,20 @@
+#' Robust BIC-type criterion
+#'
+#' Computes a BIC-type information criterion for the current fit following the
+#' proposal in Boente et al. (2020)
+#' 
+#' @param n Sample size.
+#' @param scl Scale estimate (numeric).
+#' @param val Minimization value (numeric).
+#' @param k_npt Number of parameters for the non-parametric term.
+#' @param k_ft Number of parameters for the functional term.
+#'
+#' @return Information criterion for the selected criterion.
+#'
+#' @references Boente, Graciela & Salibian-Barrera, Matias & Vena,
+#'     Pablo. (2020). Robust estimation for semi-functional linear regression
+#'     models. Computational Statistics & Data
+#'     Analysis. 152. 107041. 10.1016/j.csda.2020.107041.
+rbic <- function(n, scl, val, k_npt, k_ft) {
+    log(scl^2 * val / n) + (k_npt + k_ft) * log(n) / (n)
+}
